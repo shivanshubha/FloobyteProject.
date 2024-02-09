baf1312b5557d2024786ddacf75b6942d60e3efa
shivanshubha <shivanshubharti655@gmail.com>
2024-02-09 10:49:57 +0530

    up

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -12,7 +12,7 @@ router.post("/", createUser);
 router.get("/", getUsers);
 router.get("/:id", getUserByUserId);
 // router.patch("/", updateUsers);
-router.delete("/", deleteUser);
-router.post("/login", login);
+// router.delete("/", deleteUser);
+// router.post("/login", login);
 
 module.exports = router;
